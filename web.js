f4d9eef00246c00c20772847d8ce41f7c720f32c
Scott Dillender <sodillen@gmail.com>
2013-07-10 22:21:31 -0500

    Added index.html with proper text and formatted files for consistent spacing.

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
-var express = require('express');
+var require = require || '';
+var express = require('express'), fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+app.get('/', function (request, response) {
+    fs.readFile('index.html', function (err, data) {
+        if (err) {
+            response.send("Error reading file");
+            return;
+        }
+        response.send(data.toString());
+    });
+
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
